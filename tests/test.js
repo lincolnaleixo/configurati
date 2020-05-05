a812dcc3a221863ad7842921d9fe9bdc599c2339
lincolnaleixo <lincolnmorais@gmail.com>
2020-05-05 19:12:49 +0200

    add tests

--- tests/test.js
+++ tests/test.js
@@ -5,13 +5,23 @@ const fetch = require('node-fetch')
 const Configurat = require('../src/configurati')
 const gDriveOptions = require('./gDriveOptions.js')
 const githubRepoOptions = require('./githubRepoOptions.js')
-const type = 'githubRepo'
-const config = new Configurat(type, githubRepoOptions)
+const type = 'gsheets'
+const config = new Configurat(type, gDriveOptions)
+const Cryptologist = require('../lib/cryptologist')
 
 async function test() {
+	// const crypto = new Cryptologist()
+	// const hw = encrypt('Some serious stuff')
+	// console.log(hw)
+	// console.log(decrypt(hw))
+	// process.env.NODE_ENV = 'PRODUCTION'
 	const cfg = await config.get()
-	// await config.set(cfg)
 	console.log(cfg)
+	// await config.set(cfg)
+	// const toEncrypt = JSON.stringify(cfg)
+	// const encryptedConfig = crypto.encrypt(toEncrypt)
+	// console.log(encryptedConfig)
+	// console.log(crypto.decrypt(encryptedConfig))
 }
 
 test()
