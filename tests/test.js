7da3b9a56908da1915789992c803423bdb11e084
Lincoln <lincolnmorais@gmail.com>
2020-05-14 16:49:14 +0200

    added args tests

--- tests/test.js
+++ tests/test.js
@@ -5,23 +5,26 @@ const fetch = require('node-fetch')
 const Configurat = require('../src/configurati')
 const gDriveOptions = require('./gDriveOptions.js')
 const githubRepoOptions = require('./githubRepoOptions.js')
-const type = 'gsheets'
-const config = new Configurat(type, gDriveOptions)
-const Cryptologist = require('../lib/cryptologist')
+const Cryptologist = require('../lib/cryptologist');
 
-async function test() {
-	// const crypto = new Cryptologist()
-	// const hw = encrypt('Some serious stuff')
-	// console.log(hw)
-	// console.log(decrypt(hw))
-	// process.env.NODE_ENV = 'PRODUCTION'
+(async () => {
+	let config
+	if (process.argv.find((item) => item === '--nodeEnv')) {
+		process.env.NODE_ENV = process.argv[process.argv.findIndex((item) => item === '--nodeEnv') + 1]
+	} else {
+		process.env.NODE_ENV = 'PRODUCTION'
+		console.log('Using default NODE_ENV PRODUCTION')
+	}
+	if (process.argv.find((item) => item === '--gsheets')) {
+		config = new Configurat('gsheets', gDriveOptions)
+	} else if (process.argv.find((item) => item === '--github')) {
+		config = new Configurat('githubRepo', githubRepoOptions)
+	} else {
+		console.log('No config type valid options')
+		process.exit(0)
+	}
+
 	const cfg = await config.get()
 	console.log(cfg)
-	// await config.set(cfg)
-	// const toEncrypt = JSON.stringify(cfg)
-	// const encryptedConfig = crypto.encrypt(toEncrypt)
-	// console.log(encryptedConfig)
-	// console.log(crypto.decrypt(encryptedConfig))
-}
+})()
 
-test()
